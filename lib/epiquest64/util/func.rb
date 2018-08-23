fd911b5133c3c624248f2ce434e8530969af5cee
DarkHole1 <dark.hole1@yandex.ru>
2018-08-23 22:11:59 +0300

    toReadableMoney improvement

--- lib/epiquest64/util/func.rb
+++ lib/epiquest64/util/func.rb
@@ -1,23 +1,24 @@
 class Func
   def generateLuck
     n = rand(100)
-    
     return n >= 50 ? -1 : n >= 40 ? 0 : n >= 30 ? 1 : n >= 20 ? 2 : n >= 5 ? 3 : n >= 1 ? 4 : 5
   end
-  def toReadableMoney(copper)
-    if copper < 100
-      return "#{copper} copper"
+
+  def toReadableMoney(money)
+    copper = money % 100
+    silver = money / 100 % 100
+    gold = money / 10000 % 10
+    platinum = money / 100000
+
+    res = "#{copper} copper"
+
+    if gold > 0 then
+      res = "#{gold} gold " + res
     end
-    silver = copper / 100
-    gold = copper / 10000
-    platinum = copper / 100000
-    case
-    when silver > 0 && gold < 1
-      return "#{silver} silver"
-    when gold > 0 && platinum < 1
-      return "#{gold} gold"
-    when platinum > 0
-      return "#{platinum} platinum"
+    if platinum > 0 then
+      res = "#{platinum} platinum " + res
     end
+
+    return res
   end
 end
